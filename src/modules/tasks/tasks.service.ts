f0ba51fc643f5c927336a6b96bbcb8cce6cb7984
Andrey Chertykovtsev <chertiav@gmail.com>
2023-11-21 14:00:14 +0100

    improve errors loger

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { join } from 'path';
+import { format } from 'date-fns';
 import * as fs from 'fs';
 
 import * as config from '../../configuration/app';
@@ -12,7 +13,7 @@ export class TasksService {
 
 	@Cron(CronExpression.EVERY_DAY_AT_8PM, {
 		name: 'loggingErrorToFile',
-		timeZone: 'Europe/Kyiv',
+		timeZone: 'Europe/Copenhagen',
 	})
 	loggingErrorToFile(): void {
 		const staticPath: string = config.default().staticPath;
@@ -32,43 +33,49 @@ export class TasksService {
 			});
 		}
 
-		fs.readFile(
-			logFileNamePath,
-			(err: NodeJS.ErrnoException | null, data: Buffer): void => {
-				if (err) {
-					this.logger.error(err.message);
-					return;
-				}
-				const date: string = new Date().toISOString().replace(/:/g, '_');
-				const file: string = join(logFilePathCurrentDay, `${date}.log`);
-				const buffer: string = data.toString('utf-8');
-				const arrayData: string[] = buffer.split('\n');
-				arrayData
-					.filter((item: string): boolean => item !== '')
-					.forEach((item: string): void => {
-						const end: number = item.indexOf('stackTrace');
-						const str: string = item.slice(0, end - 2);
-						fs.appendFile(
-							file,
-							str + '}\n',
-							(err: NodeJS.ErrnoException | null): void => {
-								if (err) {
-									this.logger.error(err.message);
-									return;
-								}
-							},
-						);
-					});
-				fs.truncate(
-					logFileNamePath,
-					(err: NodeJS.ErrnoException | null): void => {
-						if (err) {
-							this.logger.error(err.message);
-							return;
-						}
-					},
-				);
-			},
-		);
+		if (fs.existsSync(logFileNamePath)) {
+			fs.readFile(
+				logFileNamePath,
+				(err: NodeJS.ErrnoException | null, data: Buffer): void => {
+					if (err) {
+						this.logger.error(err.message);
+						return;
+					}
+					const dateTime = format(new Date(), `yyyy-MM-dd HH_m_ss`).replace(
+						' ',
+						'T',
+					);
+					// const date: string = new Date().toISOString().replace(/:/g, '_');
+					const file: string = join(logFilePathCurrentDay, `${dateTime}.log`);
+					const buffer: string = data.toString('utf-8');
+					const arrayData: string[] = buffer.split('\n');
+					arrayData
+						.filter((item: string): boolean => item.includes('stackTrace'))
+						.forEach((item: string): void => {
+							const end: number = item.indexOf('stackTrace');
+							const str: string = item.slice(0, end);
+							fs.appendFile(
+								file,
+								str + '\n',
+								(err: NodeJS.ErrnoException | null): void => {
+									if (err) {
+										this.logger.error(err.message);
+										return;
+									}
+								},
+							);
+						});
+					fs.truncate(
+						logFileNamePath,
+						(err: NodeJS.ErrnoException | null): void => {
+							if (err) {
+								this.logger.error(err.message);
+								return;
+							}
+						},
+					);
+				},
+			);
+		}
 	}
 }
--- src/utils/logging-error.ts
+++ src/utils/logging-error.ts
@@ -1,29 +1,25 @@
-import { ILoggerBody } from '../common/interfaces/exception';
+import { format } from 'date-fns';
 import { join } from 'path';
+import { v4 as uuid } from 'uuid';
 import * as fs from 'fs';
 
 import * as config from '../configuration/app';
 import { CommonConstants } from '../common/constants';
 
-export const loggingError = (objectError: ILoggerBody): void => {
+export const loggingError = async (objectError: string): Promise<void> => {
+	const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
+	const logItem = `${dateTime}\t${uuid()}\t${objectError}\n`;
+
 	const staticPath: string = config.default().staticPath;
 	const filePath: string = join(staticPath, CommonConstants.LOG_FILE.PATH);
 	const fileNamePath: string = join(filePath, CommonConstants.LOG_FILE.NAME);
-	const jsonObjectError: string = JSON.stringify(objectError);
 
-	if (!fs.existsSync(filePath)) {
-		fs.mkdirSync(filePath, {
-			recursive: true,
-		});
+	try {
+		if (!fs.existsSync(filePath)) {
+			await fs.promises.mkdir(filePath);
+		}
+		await fs.promises.appendFile(fileNamePath, logItem);
+	} catch (error) {
+		console.info(`Error append file: ${error.message}`);
 	}
-	fs.appendFile(
-		fileNamePath,
-		jsonObjectError + '\n',
-		(err: NodeJS.ErrnoException | null): void => {
-			if (err) {
-				console.info(`Error append file: ${err.message}`);
-				return;
-			}
-		},
-	);
 };
